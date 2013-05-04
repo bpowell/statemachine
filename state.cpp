c1d60fd7323d42e8fc739f52f9c62da95b0ed210
Brandon Powell <bpowell29a@gmail.com>
2013-05-04 15:12:21 -0500

    Ripped out functions for statemachine to own file.

--- state.cpp
+++ state.cpp
@@ -28,33 +28,6 @@ class MenuState : public IState
 		virtual void on_enter();
 };
 
-void StateMachine::update(float dt)
-{
-	current_state->update(dt);
-}
-
-void StateMachine::render()
-{
-	current_state->render();
-}
-
-void StateMachine::change(string name)
-{
-	current_state->on_exit();
-	current_state = states[name];
-	current_state->on_enter();
-}
-
-void StateMachine::set(string name)
-{
-	current_state = states[name];
-	current_state->on_enter();
-}
-void StateMachine::add(string name, IState *state)
-{
-	states.insert(std::pair<string, IState*>(name, state));
-}
-
 void WorldState::update(float dt)
 {
 	cout << dt;
---
+++ statemachine.cpp
@@ -0,0 +1,28 @@
+#include "statemachine.h"
+
+void StateMachine::update(float dt)
+{
+	current_state->update(dt);
+}
+
+void StateMachine::render()
+{
+	current_state->render();
+}
+
+void StateMachine::change(string name)
+{
+	current_state->on_exit();
+	current_state = states[name];
+	current_state->on_enter();
+}
+
+void StateMachine::set(string name)
+{
+	current_state = states[name];
+	current_state->on_enter();
+}
+void StateMachine::add(string name, IState *state)
+{
+	states.insert(std::pair<string, IState*>(name, state));
+}
